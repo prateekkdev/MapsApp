bd3e69f2a3f71fdd22eb06b132075ab8e1603c93
Prateek Kesarwani <prateek.kesarwani@olacabs.com>
2017-06-13 16:37:19 +0530

    Playservices added, along with current location support - can't compile due to ambiguity in dependency

--- app/src/main/java/com/example/prateekkesarwani/mapsdemo/MapsActivity.java
+++ app/src/main/java/com/example/prateekkesarwani/mapsdemo/MapsActivity.java
@@ -1,14 +1,18 @@
 package com.example.prateekkesarwani.mapsdemo;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +23,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private FusedLocationProviderClient mFusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     /**
@@ -46,25 +54,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // LatLng sydney = new LatLng(-34, 151);
 
         final LatLng egl = new LatLng(12.951292, 77.639570);
-        // final LatLng smondo = new LatLng(12.821949, 77.657729);
-        LatLng bangalore = egl;
+        final LatLng smondo = new LatLng(12.821949, 77.657729);
+        LatLng bangalore = smondo;
         LatLng delhi = new LatLng(28.645340, 77.213562);
 
-        mMap.addMarker(new MarkerOptions().position(egl).title("Marker in Bangalore"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bangalore));
+        mMap.addMarker(new MarkerOptions().position(delhi).title("Marker in Delhi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
 
 
         Observable.just("")
                 .delay(10000, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(s -> {
-                    mMap.addMarker(new MarkerOptions().position(delhi).title("Marker in Delhi"));
+                .doOnNext(s -> {
+                    mMap.addMarker(new MarkerOptions().position(bangalore).title("Marker in Bangalore"));
                     // mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
                     // mMap.animateCamera(CameraUpdateFactory.newLatLng(delhi));
 
                     // Zoom level ranges from 2.0 to 21.0 (But not all locations support max zoom)
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(delhi, 12.0f));
-                });
-
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(bangalore, 12.0f));
+                })
+                .delay(10000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(s -> moveToCurrentLocation());
     }
+
+    private void moveToCurrentLocation() {
+
+        /*
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        */
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(final Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in Current Location"));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
+                        }
+                    }
+                });
+    }
+
 }
