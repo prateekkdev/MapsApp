1d2f9a2dd3da100c1bf8c4f85134fb3c571d4786
Prateek Kesarwani <prateek.kesarwani@olacabs.com>
2017-06-19 18:40:57 +0530

    It seems even custom currentlocation marker is not needed. Do we need to even make current location api call(?), or even that is default

--- app/src/main/java/com/example/prateekkesarwani/mapsdemo/MapsActivity.java
+++ app/src/main/java/com/example/prateekkesarwani/mapsdemo/MapsActivity.java
@@ -153,13 +153,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         mCurrLocationMarker.remove();
                     }
 
+                    mMap.setMyLocationEnabled();
+
                     //Place current location marker
-                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                    MarkerOptions markerOptions = new MarkerOptions();
-                    markerOptions.position(latLng);
-                    markerOptions.title("Current Position");
-                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-                    mCurrLocationMarker = mMap.addMarker(markerOptions);
+//                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                    MarkerOptions markerOptions = new MarkerOptions();
+//                    markerOptions.position(latLng);
+//                    markerOptions.title("Current Position");
+//                    // markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+//                    mCurrLocationMarker = mMap.addMarker(markerOptions);
 
                     //move map camera
                     // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,11));
