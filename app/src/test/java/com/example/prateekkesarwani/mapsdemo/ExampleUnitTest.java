d4f446875b02c334ac3c99d8bd30e26c42a41dfe
Prateek Kesarwani <prateek.kesarwani@olacabs.com>
2017-07-03 15:54:54 +0530

    Backoff basic

--- app/src/test/java/com/example/prateekkesarwani/mapsdemo/ExampleUnitTest.java
+++ app/src/test/java/com/example/prateekkesarwani/mapsdemo/ExampleUnitTest.java
@@ -2,7 +2,13 @@ package com.example.prateekkesarwani.mapsdemo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +19,52 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void performExponentialBackoff() throws Exception {
+
+
+        for (int input = 1; input <= 10; input++) {
+            final boolean result;
+            final int value = input;
+            if (input == 5 || input == 6 || input == 7) {
+                result = false;
+            } else {
+                result = true;
+            }
+
+            Observable.create(new ObservableOnSubscribe<Boolean>() {
+                @Override
+                public void subscribe(ObservableEmitter<Boolean> e) throws Exception {
+                    if (result) {
+                        e.onComplete();
+                    } else {
+                        e.onError(new Throwable());
+                    }
+                }
+            }).subscribe(new Observer<Boolean>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+
+                }
+
+                @Override
+                public void onNext(Boolean aBoolean) {
+                    System.out.println("Next for: " + value + "");
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    System.out.println("Error for: " + value + "");
+                }
+
+                @Override
+                public void onComplete() {
+                    System.out.println("Complete for: " + value + "");
+                }
+            });
+        }
     }
 }
